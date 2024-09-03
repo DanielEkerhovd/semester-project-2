7381e6a6e00b80759fcf6d96d968269227a542a6
Daniel Ekerhovd <115421350+DanielEkerhovd@users.noreply.github.com>
2024-09-03 19:48:53 +0200

    Added ESlint ignore for tailwind

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,10 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
-export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-];
+export default {
+  languageOptions: {
+    globals: globals.browser,
+  },
+  extends: [pluginJs.configs.recommended],
+  ignorePatterns: ["tailwind.config.js"], // Add this line to ignore the file
+};
