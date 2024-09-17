9fb3ce59c0b93cf5a2e912d9fcc10be33f26fddc
Daniel Ekerhovd <115421350+DanielEkerhovd@users.noreply.github.com>
2024-09-17 17:27:06 +0200

    Updated to husky 9

---
+++ src/js/api/user/login.mjs
@@ -0,0 +1,19 @@
+
+
+export default async function login(email, password) {
+  const response = await fetch('/api/user/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  });
+
+  if (!response.ok) {
+    throw new Error('Invalid email or password');
+  }
+
+  console.log(response.json());
+
+  return response.json();
+}
---
+++ src/js/router.mjs
@@ -0,0 +1,8 @@
+console.log('Hello from router.mjs');
+
+import login from "./api/user/login.mjs";
+
+const email = 'danielE@stud.noroff.no';
+const password = 'Bergen123';
+
+login(email, password)
