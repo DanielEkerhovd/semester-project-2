7a40b9e5d45af54f3082d50223f50414026c5af4
Daniel Ekerhovd <115421350+DanielEkerhovd@users.noreply.github.com>
2024-09-19 12:09:30 +0200

    fix on fetch

--- src/js/api/listing/fetch_listing.mjs
+++ src/js/api/listing/fetch_listing.mjs
@@ -1,25 +1,16 @@
-import {LISTING_PARAM} from '/src/js/api/keys.mjs';
+import { LISTING_PARAM } from '/src/js/api/keys.mjs'
 
 export default async function listingsAPI(limit = 10) {
+  const postCount = limit
 
-    const postCount = limit;
+  const url = `${LISTING_PARAM}&limit=${postCount}`
 
-    const url = `${LISTING_PARAM}&limit=${postCount}`;
-
-
-    try {
-
-
-        const response = await fetch(url);
-        const data = await response.json();
-        const listings = data.data;
-        return listings;
-    } catch (error) {
-        console.error(error);
-    }
-
-
-
-
-
-}
+  try {
+    const response = await fetch(url)
+    const data = await response.json()
+    const listings = data.data
+    return listings
+  } catch (error) {
+    console.error(error)
+  }
+};
