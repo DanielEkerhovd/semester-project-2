8e808cac2f8a7b71cd982124f4d65b637a7d0030
Daniel Ekerhovd <115421350+DanielEkerhovd@users.noreply.github.com>
2024-09-21 13:27:46 +0200

    Added user listings to profiles

--- src/js/api/user/profile.mjs
+++ src/js/api/user/profile.mjs
@@ -1,10 +1,11 @@
-import { PROFILE_BASE, PROFILE_PARAM } from '../keys.mjs'
+import { PROFILE_BASE, PROFILE_PARAM} from '../keys.mjs'
 
-export default async function getProfile(user) {
+export default async function getProfile(user, listings) {
   const token = localStorage.getItem('accessToken')
   const key = localStorage.getItem('apiKey')
 
-  let userName
+  let userName;
+  let url;
 
   if (user) {
     userName = user
@@ -12,7 +13,15 @@ export default async function getProfile(user) {
     userName = localStorage.getItem('userName')
   };
 
-  const response = await fetch(PROFILE_BASE + userName + PROFILE_PARAM, {
+  if (listings) {
+    userName += '/listings/'
+    url = `${PROFILE_BASE}${userName}${PROFILE_PARAM}&_seller=true&_bids=true&_active=true`
+  } else {
+    url = `${PROFILE_BASE}${userName}${PROFILE_PARAM}`
+  }
+;
+
+  const response = await fetch(url, {
     method: 'GET',
     headers: {
       Authorization: `Bearer ${token}`,
--- src/js/page/profile.mjs
+++ src/js/page/profile.mjs
@@ -1,19 +1,44 @@
 import buildProfile from '../components/profile.mjs'
 import fetchUser from '../api/user/profile.mjs'
+import listCard from '../components/listing/list_card.mjs'
 
 export default async function profile(loginStatus) {
   if (!loginStatus) {
     window.location.href = '/login/'
-  }
+  };
 
-  const param = new URLSearchParams(window.location.search)
-  const user = param.get('user')
+  const param = new URLSearchParams(window.location.search);
+  const user = param.get('user');
 
-  const content = await fetchUser(user)
+  const content = await fetchUser(user);
+  const profileLists = await fetchUser(user, true);
 
-  const profileContainer = document.getElementById('profile')
+  const listContent = profileLists.data;
 
-  const profile = buildProfile(content)
+  const profileContainer = document.getElementById('profile');
+  const listingsContainer = document.getElementById('profile_list');
 
-  profileContainer.appendChild(profile)
-}
+  const profile = buildProfile(content);
+
+  if (listContent.length === 0) {
+    const noListings = document.createElement('p')
+    noListings.classList.add(
+      'text-center',
+      'text-2xl',
+      'font-text',
+      'font-light',
+      'col-span-full',
+      'row-span-2',
+      'my-32'
+    )
+    noListings.innerText = 'No active listings'
+    listingsContainer.appendChild(noListings)
+  } else {
+    listContent.forEach((listing) => {
+      const card = listCard(listing)
+      listingsContainer.appendChild(card)
+    })
+  };
+
+  profileContainer.appendChild(profile);
+};
